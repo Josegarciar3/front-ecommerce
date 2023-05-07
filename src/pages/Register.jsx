3e8a774744df19f7d7bd3c34187915748ee94f66
Josegarciar3 <jose310720@icloud.com>
2023-05-06 18:30:45 -0700

    finalizado page de register y login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,68 @@
 import styled from "styled-components"
 
 const Container = styled.div`
-    
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(rgba(255,255,255,0.5),
+    rgba(255,255,255,0.5)
+    ),
+     url("https://images.pexels.com/photos/6258910/pexels-photo-6258910.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+     center;
+     background-size: cover;
+     display: flex;
+     align-items: center;
+     justify-content: center;
+`;
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color: white;
+`;
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`;
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`;
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`;
+const Agreement = styled.span`
+    font-size: 12px;
+    margin: 20px 0px;
+`;
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
 `;
 
 const Register = () => {
   return (
     <Container>
-        hola
+        <Wrapper>
+            <Title>CREATE ACCOUNT</Title>
+            <Form>
+                <Input placeholder="name"/> 
+                <Input placeholder="last name"/> 
+                <Input placeholder="username"/> 
+                <Input placeholder="email"/> 
+                <Input placeholder="password"/> 
+                <Input placeholder="confirm password"/> 
+                <Agreement>Para crear una cuenta, Doy mi consentimiento para procesar mis datos 
+                    personales de acuerdo a la <b>POLITICA DE PRIVACIDAD</b>
+                </Agreement>
+                <Button>CREATE</Button>
+            </Form>
+        </Wrapper>
     </Container>
   )
 }
